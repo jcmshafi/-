78c2c39b686b05cfb83d5df5c49bd18c59203d5e
Shafi <jcmshafi96@gmail.com>
2026-02-21 14:13:07 +0600

    feat: configure axios instance and Create API services

---
+++ services/product.service.ts
@@ -0,0 +1,17 @@
+import { api } from "./api";
+import { Product, Category } from "@/types/product";
+
+export const getProducts = async (): Promise<Product[]> => {
+  const { data } = await api.get("/products");
+  return data;
+};
+
+export const getProduct = async (id: string): Promise<Product> => {
+  const { data } = await api.get(`/products/${id}`);
+  return data;
+};
+
+export const getCategories = async (): Promise<Category[]> => {
+  const { data } = await api.get("/categories");
+  return data;
+};
